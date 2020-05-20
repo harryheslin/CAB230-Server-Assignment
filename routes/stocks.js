8f472f445133e80251e26029bca20acfcbd774c7
CAB230 <cab230@VDI-VL24B-121.qut.edu.au>
2020-05-20 14:05:38 +1000

    Two unauthenticated stock routes fully complete

--- routes/stocks.js
+++ routes/stocks.js
@@ -1,13 +1,48 @@
 var express = require('express');
-//const mysql = require('mysql');
 var router = express.Router();
 
 router.get('/symbols', function (req, res, next) {
-    res.send('This will return all stocks with an optional industry query');
-});
 
-router.get('/SYMBOL', function (req, res, next) {
-    res.send('Returns the latest data for a specific stock specified by code');
+    const industry = Object.entries(req.query).length !== 0 ? req.query.industry ? req.query.industry : "invalid" : "";
+
+    if(industry === "invalid"){
+        res.status(404).json({ error: true, message: `Invalid query parameter: only 'industry' is permitted` });
+    
+    } else {
+        req.db.from('stocks').select("name", "symbol", "industry").where('industry', 'like', `%${industry}%`).distinct()
+        .then((rows) => {
+            
+            if(rows.length === 0){
+                res.status(404).json({ error: true, message: `Industry sector not found` });
+            } else {
+                const result = rows;
+                res.status(200).json( result );
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+    });
+
+router.get(`/:symbol`, function (req, res, next) {
+
+    const dateParams = req.query.to || req.query.from ? true : false;
+    const symbol = req.params.symbol;
+    req.db.from('stocks').select("*").where('symbol', `${symbol}`).first()
+    .then((rows) => {
+        if(dateParams){
+            res.status(400).json({ error: true, message: `Date parameters only available on authenticated route /stocks/authed` });
+        }
+        if(!rows){
+            res.status(404).json({ error: true, message: `No entry for symbol in stocks database` });
+        } else { 
+            res.status(200).json( rows );
+        }
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 });
 
 router.get('/authed/SYMBOL', function (req, res, next) {
